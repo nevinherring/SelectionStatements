d0a32c1535ccd01ec91295b726854a058650a8d2
Nevin Herring <nevin.herring@gmail.com>
2021-02-15 21:52:09 -0600

    added switch statement

--- Program.cs
+++ Program.cs
@@ -26,6 +26,29 @@ namespace SelectionStatements
             {
                 WriteLine("o is not an int so it cannot multiply!");
             }
+        A_label:
+            var number = (new Random()).Next(1, 7);
+            WriteLine($"My random number is {number}");
+            switch (number)
+            {
+                case 1:
+                    WriteLine("One");
+                    break; // jumps to end of switch statement
+                case 2:
+                    WriteLine("two");
+                    goto case 1;
+                case 3:
+                case 4:
+                    WriteLine("Three or four");
+                    goto case 1;
+                case 5:
+                    // go to sleep for half a second
+                    System.Threading.Thread.Sleep(500);
+                    goto A_label;
+                default:
+                    WriteLine("Default");
+                    break;
+            } // end of switch statement
         }
     }
 }
