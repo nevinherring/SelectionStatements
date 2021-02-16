6057a0e38a66a864c425a74a1363f04c8881e575
Nevin Herring <nevin.herring@gmail.com>
2021-02-15 21:45:02 -0600

    types

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace SelectionStatements
 {
@@ -6,7 +7,25 @@ namespace SelectionStatements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length == 0)
+            {
+                WriteLine($"There are no arguments.");
+            }
+            else
+            {
+                WriteLine("There is at lease one argument.");
+            }
+            // add and remove the "" to change the behavior
+            object o = 3;
+            int j = 4;
+            if (o is int i)
+            {
+                WriteLine($"{i} x {j}= {i * j}");
+            }
+            else
+            {
+                WriteLine("o is not an int so it cannot multiply!");
+            }
         }
     }
 }
